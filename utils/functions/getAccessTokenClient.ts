df57cfdfe6c4da4178d426afd746e9d77619457e
dangvietquang59 <dvquang5902@gmail.com>
2024-10-08 17:23:00 +0700

    fix:blog

--- utils/functions/getAccessTokenClient.ts
+++ utils/functions/getAccessTokenClient.ts
@@ -1,8 +1,11 @@
 import Cookies from 'js-cookie';
-
 import variables from '../constants/variables';
 
 export const getAccessTokenClient = () => {
+    if (typeof window === 'undefined') {
+        return null;
+    }
+
     const accessToken = Cookies.get(variables.ACCESS_TOKEN);
 
     if (accessToken) {
