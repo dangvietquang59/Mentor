35d62c492c11c8c5d0706a243494422d4c4a8bdc
dangvietquang59 <dvquang5902@gmail.com>
2024-12-24 09:31:33 +0700

    fix: translate page

--- app/failed/page.tsx
+++ app/failed/page.tsx
@@ -11,17 +11,17 @@ const FailedPage = () => {
                 <Avatar src={images.fail.src} alt="icon" size={100} />
             </div>
             <h1 className="mb-4 text-center text-[2.4rem] font-bold">
-                Payment Failed!
+                Thanh toán thất bại!
             </h1>
             <p className="mb-6 text-[1.4rem]">
-                Unfortunately, your transaction could not be completed. Please
-                try again or contact support if the issue persists.
+                Rất tiếc, giao dịch của bạn không thể thực hiện. Vui lòng thử
+                lại hoặc liên hệ hỗ trợ nếu vấn đề vẫn tiếp diễn.
             </p>
             <button
                 className="rounded-md bg-white px-6 py-2 font-semibold text-[#0F0F0F] transition duration-300 hover:bg-gray-300"
                 onClick={() => (window.location.href = '/')}
             >
-                Back to Home
+                Về trang chủ
             </button>
         </div>
     );
